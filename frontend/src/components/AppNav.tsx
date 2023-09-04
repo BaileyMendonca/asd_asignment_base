715064c943cfad00f36e5f0a9bd97f1020e85b78
Bailey Mendonca <bailey@propcode.com.au>
2023-09-04 14:09:00 +1000

    Setting up the SDK

---
+++ frontend/src/components/AppNav.tsx
@@ -0,0 +1,6 @@
+import Navigation from "./Navigation";
+
+const AappNav = () => {
+  return <Navigation />;
+};
+export default AappNav;
---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Navigation: React.FC = () => {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+
+  // Image source
+  const profileImg =
+    isAuthenticated && user?.picture ? user.picture : "/defaultProfilePic.jpg";
+
+  return (
+    <AppBar position="sticky" color="primary">
+      <Toolbar>
+        {/* Left Side - Profile Picture */}
+        <IconButton edge="start" color="inherit" aria-label="profile-picture">
+          <img
+            src={profileImg}
+            alt="Profile"
+            style={{ width: "40px", borderRadius: "50%" }}
+          />
+        </IconButton>
+        <div style={{ flexGrow: 1 }}></div>{" "}
+        {/* Spacer to push items to the right */}
+        {/* Right Side - Login or Logout Button */}
+        {isAuthenticated ? (
+          <Button
+            color="inherit"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </Button>
+        ) : (
+          <Button color="inherit" onClick={() => loginWithRedirect()}>
+            Log In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navigation;
