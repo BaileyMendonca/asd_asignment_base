d2b399e68a4e587224ffad6af6de9beb9773c1a5
Bailey Mendonca <bailey@propcode.com.au>
2023-10-10 13:21:53 +1100

    Small change so that db should work correctly

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     super({
       datasources: {
         db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-    });
-  }
-  
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          //url: process.env.DATABASE_URL,
-          url: 'postgresql://ebroot:ebroot@awseb-e-n2unv8immz-stack-awsebrdsdatabase-yobdbembudcj.csul9by1mye5.us-east-1.rds.amazonaws.com:5432/nest?schema=public',
+          url:
+            process.env.DATABASE_URL ||
+            'postgresql://ebroot:ebroot@awseb-e-n2unv8immz-stack-awsebrdsdatabase-yobdbembudcj.csul9by1mye5.us-east-1.rds.amazonaws.com:5432/nest?schema=public',
         },
       },
     });
   }
 }
-
